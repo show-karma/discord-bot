8100e99928d02eab4467f6a75b9926964e617830
Arthh <arthur.ulhoa28@gmail.com>
2023-10-05 11:23:26 -0300

    feat: twitter criteria for apecoin role job

--- src/services/apecoin/index.ts
+++ src/services/apecoin/index.ts
@@ -42,6 +42,7 @@ async function fetchDelegates(daoName: string, publicAddress?: string) {
   WHERE "t1"."daoName" = $1
     AND "t2"."period" = '1y'
     AND "t1"."discordHandle" IS NOT NULL
+    AND "u"."twitterHandle" IS NOT NULL
     `;
 
   if (publicAddress) {
@@ -81,7 +82,6 @@ async function delegateHasPermission(delegate, dao) {
     delegate.hasPermission = delegate.hasPermission || delegate.trustLevel >= 2;
   }
 
-  delegate.hasPermission = false;
   return delegate;
 }
 
